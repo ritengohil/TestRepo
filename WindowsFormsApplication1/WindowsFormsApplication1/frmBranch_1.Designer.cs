133fc6e051346608772dcc62fe9bc5ec76053785
ritengohil <rgohil@webuy.com>
2015-09-24 17:45:23 +0530

    changes in branch-1

--- WindowsFormsApplication1/WindowsFormsApplication1/frmBranch_1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/frmBranch_1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.lblMessage = new System.Windows.Forms.Label();
             this.btCloseBranch1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblMessage
@@ -53,11 +54,21 @@
             this.btCloseBranch1.UseVisualStyleBackColor = true;
             this.btCloseBranch1.Click += new System.EventHandler(this.btCloseBranch1_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(151, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
             // frmBranch_1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(297, 262);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btCloseBranch1);
             this.Controls.Add(this.lblMessage);
             this.Name = "frmBranch_1";
@@ -71,5 +82,6 @@
 
         private System.Windows.Forms.Label lblMessage;
         private System.Windows.Forms.Button btCloseBranch1;
+        private System.Windows.Forms.Label label1;
     }
 }
